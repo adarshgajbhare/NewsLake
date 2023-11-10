f8c1e4d90dd31b95f82a18709506fc464831940c
adarshgajbhare <adarshgajbhare@gmail.com>
2023-11-10 21:39:35 +0530

    Finally fixed the error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     setSelectedCountry(country);
   };
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/https://flareflashfeed.netlify.app'>
       <div>
         <Navbar country={selectedCountry} onCountryChange={handleCountryChange} />
         <Routes>
